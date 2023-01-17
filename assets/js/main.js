ab85ed20c6059d0da1f4ba1ec473785afdab6e07
diogoka <diogokalmeida@gmail.com>
2023-01-17 08:29:36 -0800

    Template added. Customization of the front page.

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $header = $("#header"),
+    $footer = $("#footer"),
+    $main = $("#main"),
+    settings = {
+      parallax: true,
+      parallaxFactor: 20,
+    };
+
+  breakpoints({
+    xlarge: ["1281px", "1800px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: [null, "480px"],
+  });
+
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
+
+  if(browser.mobile) {
+    $body.addClass("is-touch");
+
+    window.setTimeout(function () {
+      $window.scrollTop($window.scrollTop() + 1);
+    }, 0);
+  }
+
+  breakpoints.on("<=medium", function () {
+    $footer.insertAfter($main);
+  });
+
+  breakpoints.on(">medium", function () {
+    $footer.appendTo($header);
+  });
+
+  if (browser.name == "ie" || browser.mobile) settings.parallax = false;
+
+  if (settings.parallax) {
+    breakpoints.on("<=medium", function () {
+      $window.off("scroll.strata_parallax");
+      $header.css("background-position", "");
+    });
+
+    breakpoints.on(">medium", function () {
+      $header.css("background-position", "left 0px");
+
+      $window.on("scroll.strata_parallax", function () {
+        $header.css(
+          "background-position",
+          "left " +
+            -1 * (parseInt($window.scrollTop()) / settings.parallaxFactor) +
+            "px"
+        );
+      });
+    });
+
+    $window.on("load", function () {
+      $window.triggerHandler("scroll");
+    });
+  }
+})(jQuery);
