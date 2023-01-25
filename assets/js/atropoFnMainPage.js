88838f408f70a5a30c21d4ad53919fb8b81dc10b
diogoka <diogokalmeida@gmail.com>
2023-01-24 17:23:36 -0800

    Updating layout, adding effects.

---
+++ assets/js/atropoFnMainPage.js
@@ -0,0 +1,40 @@
+let title = document.querySelector('.profile')
+let icons = document.querySelector('.icons')
+
+const profile = Atropos({
+    el: '.profile-atropos',
+    activeOffset: 40,
+    shadowScale: 1.05,
+    onEnter() {
+        title.style.transform = 'scale(1.15)'
+
+
+    },
+    onLeave() {
+        title.style.transform = 'scale(1)'
+
+    },
+    onRotate(x, y) {
+
+    }
+});
+
+const footer = Atropos({
+    el: '.foooter-atropos',
+    activeOffset: 40,
+    shadowScale: 1.05,
+    rotateXMax: 0,
+    rotateYMax: 0,
+    shadow: false,
+
+    onEnter() {
+        icons.style.transform = 'scale(1.15)'
+    },
+    onLeave() {
+        icons.style.transform = 'scale(1)'
+
+    },
+    onRotate(x, y) {
+
+    }
+});
---
+++ pages/taskApp/assets/js/modalTask.js
@@ -0,0 +1,14 @@
+var modal = document.getElementById("taskModal");
+var btn = document.getElementById("demonstrationModal");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
